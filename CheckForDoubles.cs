969dfaca57abfb5d0ac100dde5a248d131538639
Mike Fossel <mikefossel@gmail.com>
2016-03-30 21:10:23 -0700

    initial

---
+++ CheckForDoubles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        public static bool Double_check(string strng)
+        {
+            strng = strng.ToLower();
+            for (int i=1;i<strng.Length;i++)
+            {
+                char last = strng[i-1];
+                char current = strng[i];
+                if (last == current)
+                {
+                    return true;
+                }
+              
+            }
+
+            return false;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a string:");
+                string input = Console.ReadLine();
+                Console.WriteLine(Double_check(input));
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ RemoveTheMin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    class Program
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+
+            if (numbers.Count == 0)
+            {
+                return numbers;
+            }
+
+            int smallest = numbers[0];
+            int j = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+
+                if (numbers[i] < smallest)
+                {
+                    smallest = numbers[i];
+                    j = i;
+                }
+
+            }
+            numbers.RemoveAt(j);
+
+            return numbers;
+
+        }
+
+    }
+
+}
+
+
